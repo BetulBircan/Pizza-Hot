4ebbf0584cdeee047ca629662fd60fc9e845460e
Betül Bircan <betul.bircan@fairteknoloji.com.tr>
2026-09-08 00:44:33 +0300

    PizzaList için http get isteği yapıldı liste dinamik olarak alındı

--- pizza-hot/src/components/PizzaList.jsx
+++ pizza-hot/src/components/PizzaList.jsx
@@ -1,17 +1,34 @@
 import Pizza from "./Pizza";
+import { useState, useEffect } from "react";
 
 export default function PizzaList() {
+
+  const [loadedPizzas, setLoadedPizzas] = useState([]);
+
+  useEffect(() => {
+      const getPizzas = async () => {
+    const response = await fetch("http://localhost:3000/pizzas");
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch pizzas");
+    }
+
+    const pizzas = await response.json();
+    setLoadedPizzas(pizzas);
+
+  }
+
+  getPizzas();
+  }, []);
+
   return (
     <div className="pizza-list">
       <div className="row row-cols-2 row-cols-md-3 row-cols-xl-4 g-4">
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
-        <Pizza />
+        {
+          loadedPizzas.map((pizza) => (
+            <Pizza key={pizza.id} />
+          ))
+        }
       </div>
     </div>
   );
